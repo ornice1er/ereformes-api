d93051b86d73b137d2d6f2ffe97f6ed6baaf7f7e
Wisdom2004 <wisdomegbogbe2004@gmail.com>
2025-06-13 16:11:27 +0100

    Reformes et dashboard

--- app/Http/Repositories/DashboardRepository.php
+++ app/Http/Repositories/DashboardRepository.php
@@ -121,5 +121,44 @@ class DashboardRepository
         return $data;
     }
 
+    public function storeFile(Request $request)
+    {
+        $datas=$request->all();
+        $validator = Validator::make($datas, [
+          //  "file" => "required|mimes:application/pdf"
+            "file" => "required"
+        ],
+        [
+            'file.required' => 'Le fichier est requis',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 500);
+        }
+
+
+        $filename='';
+        if($request->file('file')) {
+        $filename=FileStorage::setFile('public',$request->file('file'),'reformes-docs', Str::slug($request->name.date("ymdhis")));
+        $media=Media::create([
+            "chemin"=>env('APP_URL')."/storage/reformes-docs/".$filename,
+            "name"=>$filename
+        ]);
+
+
+        return response()->json([
+            "success"=>true,
+            "message"=>"Stats",
+            "data"=>$media
+        ],200);
+        } else {
+            return response()->json([
+                "success"=>true,
+                "message"=>"Stats",
+            ],500);        }
+
+
+
+    }
 
 }
