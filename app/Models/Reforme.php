a70eef5352a0b5bf29b80eb3776c0035c00389db
Ornice <ornihouss1@gmail.com>
2025-10-17 18:34:34 +0100

    fix data

--- app/Models/Reforme.php
+++ app/Models/Reforme.php
@@ -11,6 +11,8 @@ class Reforme extends Model
     use HasFactory, Filterable;
     protected $guarded = [];
     public $timestamps = false;
+        private static $whiteListFilter = ['*'];
+
 
     public function affectations()
     {
